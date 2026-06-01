c0e4ede574843ad38516557c036fa22585c2c472
Marco Castañeda <marco_pascual410@hotmail.com>
2026-06-01 14:09:20 -0600

    remove api images link

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,6 +1,5 @@
 export const API = import.meta.env.VITE_BACKEND
 export const VITE_BACKEND_SOCKET_IO = import.meta.env.VITE_BACKEND_SOCKET_IO
-export const API_IMAGES_BASE_URL = import.meta.env.VITE_BACKEND_IMAGES_BASE_URL
 export const FRONTEND_DOMAIN = import.meta.env.VITE_FRONTEND_DOMAIN
 
 export const iconStroke = 1.5;
